da9e181b626cbf15ea27b550d1cc4533f5f50d8b
hammettLOL <mavertavara@gmail.com>
2017-08-01 16:12:25 -0500

    MAin Acticity upload 2

--- app/src/main/java/com/labs/maverick/a116/activities/MainActivity.java
+++ app/src/main/java/com/labs/maverick/a116/activities/MainActivity.java
@@ -5,13 +5,16 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.labs.maverick.a116.R;
 
 public class MainActivity extends AppCompatActivity {
+    private Button memergenciaButton;
 
-    private TextView mTextMessage;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -20,13 +23,16 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    memergenciaButton.setVisibility(View.VISIBLE);
+
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    memergenciaButton.setVisibility(View.INVISIBLE);
+
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    memergenciaButton.setVisibility(View.INVISIBLE);
+
                     return true;
             }
             return false;
@@ -37,8 +43,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
+        memergenciaButton = findViewById(R.id.emergencyButton);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
