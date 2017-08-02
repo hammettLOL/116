03ac38c12bc0ba31d3666b443aa494dee93c9040
hammettLOL <mavertavara@gmail.com>
2017-08-01 23:44:08 -0500

    MAin Acticity upload dimensions

--- app/src/main/java/com/labs/maverick/a116/activities/SolicitActivity.java
+++ app/src/main/java/com/labs/maverick/a116/activities/SolicitActivity.java
@@ -1,5 +1,6 @@
 package com.labs.maverick.a116.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,7 +21,7 @@ public class SolicitActivity extends AppCompatActivity {
         msolicitnowButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                startActivity(new Intent(SolicitActivity.this,SolicitHelpActivity.class));
             }
         });
     }
